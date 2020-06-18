a1519f65634717e7d88fbebb9e82f91f3a2bb327
vaibhav dhiman <princedhiman70@gmail.com>
2020-06-18 13:04:14 +0530

    adding recipe to page

--- recipe/src/app/recipes/recipes-list/recipes-list.component.ts
+++ recipe/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    // tslint:disable-next-line: max-line-length
+    new Recipe('A test recipe', 'this is desc', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg')
+  ];
+
   constructor() { }
 
   ngOnInit() {
